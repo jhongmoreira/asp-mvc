bf8b46de1a7d9786eee3bc36c7639d3f7c85e0b6
Jhonathan Moreira <arte.jhonathan@gmail.com>
2023-04-06 13:39:44 -0300

    First Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using estudo_mvc.Models;
+
+namespace estudo_mvc.Controllers;
+
+public class HomeController : Controller{
+
+    public IActionResult Index()
+    {
+        HomeModel home = new HomeModel();
+
+        home.Nome = "Jhonathan Moreira";
+        home.Email = "arte.jhonathan@gmail.com";
+
+        return View(home);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
